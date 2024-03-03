cf4e800b3568610b927de4f66f13c41eb29fa93e
amuseqerns <119254472+amuseqerns@users.noreply.github.com>
2024-03-03 20:52:32 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Fern-cafe/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        'bang-blue':'#D9D9D9',
+        'unselect':'#FFD6B0'
+
+      }
+    },
+  },
+  plugins: [],
+}
+
